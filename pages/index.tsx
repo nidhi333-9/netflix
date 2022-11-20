30fbc4ad9d28f36d6619955c288f38357520cf71
nidhikumari <kumari9936nidhi@gmail.com>
2022-11-20 19:15:20 +0530

    redering is working now

--- pages/index.tsx
+++ pages/index.tsx
@@ -42,12 +42,35 @@ export default function Home({
       <Header />
       <main className="relative pl-5 pb-24 lg:space-y-24 lg:pl-16">
         <Banner netflixOriginals={netflixOriginals} />
-        <section>
-          <Row />
-          <Row />
-          <Row />
-          <Row />
-          <Row />
+        <section className="md:space-y-24">
+          <Row
+            title="Trending Now"
+            movies={trendingNow}
+          />
+          <Row
+            title="Top Rated"
+            movies={topRated}
+          />
+          <Row
+            title="Action Thrillers"
+            movies={actionMovies}
+          />
+          <Row
+            title="Comedies"
+            movies={comedyMovies}
+          />
+          <Row
+            title="Scary Movies"
+            movies={horrorMovies}
+          />
+          <Row
+            title="Romance Movies"
+            movies={romanceMovies}
+          />
+          <Row
+            title="Documentries"
+            movies={documentaries}
+          />
         </section>
       </main>
     </div>
