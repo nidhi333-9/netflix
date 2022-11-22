1b9aa57e9f8ca83a0e5c1a571fa504621723573e
nidhikumari <kumari9936nidhi@gmail.com>
2022-11-22 09:09:08 +0530

    initialize with firebase

---
+++ firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCxLHLB9srwFM7bufa_ty4YZoEMRleueE8",
+  authDomain: "netflix-3cfe7.firebaseapp.com",
+  projectId: "netflix-3cfe7",
+  storageBucket: "netflix-3cfe7.appspot.com",
+  messagingSenderId: "14824875126",
+  appId: "1:14824875126:web:df963912831c3849e0ab57",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
